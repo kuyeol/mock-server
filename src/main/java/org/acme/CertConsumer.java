63be4a9eb66c68b73d57b04088de5e82df85b243
kuyeol <zimop12@gmail.com>
2026-02-18 13:09:42 +0900

    deploy: 2026-02-18 13:09:42

--- src/main/java/org/acme/CertConsumer.java
+++ src/main/java/org/acme/CertConsumer.java
@@ -1,12 +1,20 @@
 package org.acme;
 
+import io.smallrye.mutiny.Uni;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.acme.model.StoredKeyPair;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-@Path("")
+@Path("api/key")
 public class CertConsumer {
+    private static final Logger log = LoggerFactory.getLogger(CertConsumer.class);
+
+
 
 @GET
 @Produces(MediaType.APPLICATION_JSON)
@@ -14,7 +22,10 @@ public String ListenCert() {
     return "hello";
 
 }
-
+    @POST
+    public void listenKey(StoredKeyPair key){
+    log.info(" received {}",key);
+    }
 
 
 }
---
+++ src/main/java/org/acme/model/StoredKeyPair.java
@@ -0,0 +1,50 @@
+package org.acme.model;
+
+import java.util.Objects;
+
+public class StoredKeyPair {
+
+    private final String id;
+    private final String serviceName;
+    private final String serialNumber;
+    private final Long expiration;
+    private final byte[] keyStore;
+    private final byte[] trustkey;
+
+
+
+    public StoredKeyPair(String id, String serviceName, String serialNumber, Long expiration, byte[] keyStore, byte[] trustkey) {
+        this.id = Objects.requireNonNull(id, "ID는 필수입니다.");
+        this.serviceName = Objects.requireNonNull(serviceName, "ServiceName은 필수입니다.");
+        this.serialNumber = Objects.requireNonNull(serialNumber, "S/N is Null");
+        this.keyStore = Objects.requireNonNull(keyStore, "keystore os null");
+        this.trustkey = Objects.requireNonNull(trustkey, "truststore is null");
+        this.expiration = expiration;
+    }
+
+    // 3. Getter 메서드만 제공 (Setter 없음)
+    public String getId() {
+        return id;
+    }
+
+    public String getServiceName() {
+        return serviceName;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public Long getExpiration() {
+        return expiration;
+    }
+
+    public byte[] getKeyStore() {
+        return keyStore;
+    }
+
+    public byte[] getTrustStore() {
+        return trustkey;
+    }
+
+}
