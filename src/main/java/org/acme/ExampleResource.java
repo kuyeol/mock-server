8227dddd4dd65ea11ccc5d469b3b3b2483292d0e
kuyeol <zimop12@gmail.com>
2026-02-18 13:44:49 +0900

    deploy: 2026-02-18 13:44:49

--- src/main/java/org/acme/ExampleResource.java
+++ src/main/java/org/acme/ExampleResource.java
@@ -5,13 +5,13 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/hello")
+@Path("/health/live")
 public class ExampleResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello from Quarkus REST";
+        return "Running";
     }
 
 }
